c39788788271404493fd31c94ccfa52ff3288a89
7thFox <dev7thFox@gmail.com>
2017-09-26 07:41:04 -0500

    Fixed uploading my token for the world to see

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-	"strings"
 	"hypothesisbot/command"
 	"hypothesisbot/sender"
+	"io"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+func getToken() string {
+	buf := bytes.NewBuffer(nil)
+	f, _ := os.Open("token") // Error handling elided for brevity.
+	io.Copy(buf, f)          // Error handling elided for brevity.
+	f.Close()
+	s := string(buf.Bytes())
+
+	return s
+}
+
 func main() {
-	discord, err := discordgo.New("Bot Mjc1OTgzOTg3NjY1NTM0OTc2.DEbucA.8LUP6jTUXG-wNG206aiNtJuME2o")
+	discord, err := discordgo.New("Bot " + getToken()) // No more pushing code with my token
 	if err != nil {
 		fmt.Println("Err ", err)
 		return
@@ -39,7 +51,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	sender := sender.NewSender(s,m)
+	sender := sender.NewSender(s, m)
 
 	if strings.HasPrefix(m.Content, "!say") {
 		cmd, _ := command.ParseCommand(m)
