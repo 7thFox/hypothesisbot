3726b6f9a11ab9f10378a4e1fa425a722221bc57
7thFox <dev7thFox@gmail.com>
2017-09-26 19:09:09 -0500

    Added Update to sender

---
+++ sender/update.go
@@ -0,0 +1,20 @@
+package sender
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+// Update receives strings and updates the message until the exit signal is received
+func (s Sender) Update(m chan string, ex chan int) {
+	var msg *discordgo.Message
+	select {
+	case ms := <-m:
+		if msg == nil {
+			msg, _ = s.session.ChannelMessageSend(s.channelid, ms)
+		} else {
+			s.session.ChannelMessageEdit(s.channelid, msg.ID, ms)
+		}
+	case <-ex:
+		return
+	}
+}
