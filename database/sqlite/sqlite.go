5c50a18ae46724e48dc0c337fd6a487139519604
7thFox <dev7thFox@gmail.com>
2017-11-02 11:51:26 -0500

    Added sqlite db

--- database/sqlite/sqlite.go
+++ database/sqlite/sqlite.go
@@ -1,16 +1,45 @@
 package sqlite
 
-import "github.com/bwmarrin/discordgo"
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type Sqlite struct {
+	db *sql.DB
 }
 
 func NewSqlite(l string) *Sqlite {
-	db := Sqlite{}
-	return &db
+	ls := Sqlite{}
+	db, err := sql.Open("sqlite3", l)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ls.db = db
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS foo (s STRING);")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return &ls
 }
 
 func (db *Sqlite) LogMessage(m *discordgo.MessageCreate) {
-	fmt.Println(m.Content)
+
+	tx, err := db.db.Begin()
+	if err != nil {
+		fmt.Println(err)
+	}
+	stmt, err := tx.Prepare("INSERT INTO foo (s) VALUES (?);")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer stmt.Close()
+
+	stmt.Exec(m.Content)
+	tx.Commit()
 }
