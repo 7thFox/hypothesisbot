7a618e2a257425f9bfeb524090f356a211553d52
7thFox <dev7thFox@gmail.com>
2018-01-06 10:54:33 -0600

    Added more error logging including a ping to database conncetion

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/7thFox/hypothesisbot/database"
 )
@@ -11,6 +12,10 @@ func (c *Config) Database() database.Database {
 		db, err := database.NewDatabase(c.dbType(), c.dbHost(), c.dbName())
 		if err != nil {
 			fmt.Printf("\nError getting database: %s\n", err.Error())
+			fmt.Printf("Host: %s, Name: %s Type: %s\n", c.dbHost(), c.dbName(), c.dbType())
+			out, _ := exec.Command("ping", c.dbHost(), "-c 5", "-w 10").Output()
+			fmt.Println(string(out))
+			return nil
 		}
 		c.db = db.(database.Database)
 	}
