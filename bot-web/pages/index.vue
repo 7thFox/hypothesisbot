515454e24ec481567a200611385df566208e80d8
7thFox <dev7thFox@gmail.com>
2018-01-21 11:28:36 -0600

    nuxt+vuetify initial setup

--- bot-web/pages/index.vue
+++ bot-web/pages/index.vue
@@ -1,64 +1,15 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        bot-web
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <img src="/avatar.png" class="mb-5" />
       </div>
-    </div>
-  </section>
+      <v-card>
+        <v-card-title class="headline">Welcome</v-card-title>
+        <v-card-text>
+          <p>text here</p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ bot-web/pages/inspire.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+      <blockquote class="blockquote">
+        &#8220;Then, solve the problems you just created.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Me</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
